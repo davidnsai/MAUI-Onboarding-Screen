f5119e31b0390ad7b3ad667d2ed4e786a47c7c4d
davidnsai <nsaidave@gmail.com>
2024-07-23 15:24:33 +0200

    Completed Project

---
+++ Onboarding/Models/OnBoardingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onboarding.Models
+{
+    public class OnBoardingModel
+    {
+        public string IntroTitle { get; set; }
+        public string IntroImage { get; set; }
+        public string IntroDescription { get; set; }
+    }
+}
